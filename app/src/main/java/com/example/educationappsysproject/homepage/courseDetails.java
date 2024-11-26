aa4e3c24e07e9db3adfb0692fd57f771a942ab04
Shawcha <mahamudulislamshawcha2007040@gmail.com>
2024-11-26 13:41:10 +0600

    video , pdf and exam section buttons added

--- app/src/main/java/com/example/educationappsysproject/homepage/courseDetails.java
+++ app/src/main/java/com/example/educationappsysproject/homepage/courseDetails.java
@@ -1,9 +1,12 @@
 package com.example.educationappsysproject.homepage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,15 +15,38 @@ import com.example.educationappsysproject.R;
 
 public class courseDetails extends AppCompatActivity {
 
+
+    CardView video , exam , pdf;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_course_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        video = findViewById(R.id.videoCard);
+        exam = findViewById(R.id.examCard);
+        pdf = findViewById(R.id.pdfCard);
+
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(courseDetails.this , courseDetails.class);
+                startActivity(i);
+            }
+        });
+        exam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(courseDetails.this , courseDetails.class);
+                startActivity(i);
+            }
+        });
+
+        pdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(courseDetails.this , courseDetails.class);
+                startActivity(i);
+            }
         });
     }
 }
